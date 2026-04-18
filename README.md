d0ce888462e9a819989ae1d430b249c4a38fedec
mithun <m.mithun222608@gmail.com>
2026-04-18 13:35:16 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 3.2_digital_watch
---
+++ script.js
@@ -0,0 +1,10 @@
+let hours = document.querySelector(".hrs");
+let min = document.querySelector(".min");
+let sec = document.querySelector(".sec");
+
+setInterval(()=>{
+    let currentTime = new Date();
+hours.innerHTML =  (currentTime.getHours()<10?'0':'') + currentTime.getHours();
+min.innerHTML =  (currentTime.getMinutes()<10?'0':'') + currentTime.getMinutes();
+sec.innerHTML =  (currentTime.getSeconds()<10?'0':'') +currentTime.getSeconds();
+},1000)
